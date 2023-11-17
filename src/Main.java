5172c9eee46dff38c0d6c1393b57d49d36645982
vansgog <0314513@gmail.com>
2023-11-17 17:07:40 +0300

    practicing JavaDoc

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,26 @@
 public class Main {
     public static void main(String[] args) {
+        /** this is a programm suggesting a name for a group of thiefs **/
         System.out.print("Hello ");
         System.out.println("World!");
 
         System.out.println("Ideas for the name of our gang:");
-        System.out.println("Byte Bandits\n" +
+        System.out.println("Byte Bandits\n" + // best one in my opinion
                 "Array Bandits\n" +
-                "CryptoKey Crooks\n" +
+                "CryptoKey Crooks\n" + // FIXME: remove this option
                 "Boolean Burglars\n" +
+                /* possibly
+                all
+                the
+                options
+                are
+                bad */
                 "Cunning Coders\n" +
                 "Assembler Hustlers\n" +
                 "Blockchain Brigade\n" +
                 "Java Gents\n" +
                 "La Kotlin Cartel");
+        // TODO: choose only one name
         System.err.printf("Error");
     }
 }
