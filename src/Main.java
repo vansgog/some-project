095ac8d5c085214f991a55d0ee79de9709aa2254
George Maysuradze <145342865+vansgog@users.noreply.github.com>
2023-11-17 16:43:10 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello ");
+        System.out.println("World!");
+
+        System.out.println("Ideas for the name of our gang:");
+        System.out.println("Byte Bandits\n" +
+                "Array Bandits\n" +
+                "CryptoKey Crooks\n" +
+                "Boolean Burglars\n" +
+                "Cunning Coders\n" +
+                "Assembler Hustlers\n" +
+                "Blockchain Brigade\n" +
+                "Java Gents\n" +
+                "La Kotlin Cartel");
+        System.err.printf("Error");
+    }
+}
